ae181525ad713fbfa1c67d7d4f232db52d0cc928
coderock100 <vd.ssaathvik@gmail.com>
2024-10-26 20:12:21 +0530

    looptime

--- looptime.py
+++ looptime.py
@@ -1,11 +1,16 @@
-def function(n):
-    for i in range(0, n + 1):  # Loop 1
-        print("First Loop")
+def myfunction(n):
 
+    for i in range(0, n + 1):
+        print("First Loop")
+    
     j = 1
-    while j <= n + 1:  # Loop 2
-        print("Second Loop ", j)
+    while j <= n + 1:
+        print("Second Loop", j)
         j = j * 2
 
-    for i in range(0, 100):  # Loop 3
-        print("Third loop")
+    for i in range(0, 100):
+        print("Third Loop")
+
+myfunction(4)
+
+print("Final value of i:", )
